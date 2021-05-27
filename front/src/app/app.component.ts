0c6d6f6dcad67152d0633e6faabed1ea6cc94bc7
JeremyAMichel <jeremy.a.michel@hotmail.fr>
2021-05-27 15:00:39 +0200

    Exercice 4

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -8,4 +8,26 @@ import { Statistique } from 'src/models/statistique';
 })
 export class AppComponent {
   title = 'Examen-Angular';
+  public tabStats: Statistique[] = [
+    {
+      id: 'fa1f5f39-bf3b-114b-9aec-7f5822ecfb46',
+      titre: 'Nombre de Tongues en France',
+      valeur: '300000000',
+      appreciation: 'WARNING',
+    },
+    {
+      id: 'fa1f5f40-be3b-11eb-91ec-7f5875ecfb46',
+      titre: 'Démographie en France',
+      valeur: '60M',
+      appreciation: 'SUCCESS',
+    },
+  ];
+
+  constructor() {
+    setTimeout(() => {
+      this.tabStats.push(
+        new Statistique('hfihlhgozhem', 'ez pz', '1', 'DANGER')
+      );
+    }, 3000);
+  }
 }
--- front/src/app/print-statistique/print-statistique.component.ts
+++ front/src/app/print-statistique/print-statistique.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Statistique } from 'src/models/statistique';
 
 @Component({
@@ -7,18 +7,10 @@ import { Statistique } from 'src/models/statistique';
   styleUrls: ['./print-statistique.component.css'],
 })
 export class PrintStatistiqueComponent implements OnInit {
-  public stat1: Statistique = new Statistique(
-    'fa1f5f40-be3b-11eb-91ec-7f5875ecfb46',
-    'Démographie en France',
-    '60M',
-    'SUCCESS'
-  );
-  public stat2: Statistique = new Statistique(
-    'fa1f5f39-bf3b-114b-9aec-7f5822ecfb46',
-    'Nombre de Tongues en France',
-    '300000000',
-    'WARNING'
-  );
+  @Input() public stat!: Statistique;
+
+  /// CORRIGE DE L'EXERCICE 3 SUITE A UNE PETITE ERREUR DE PARCOUS
+  /// on utilise bien les inputs cette fois-ci et le composant statistique affiche bien UNE SEULE stat ;)
 
   constructor() {}
 
